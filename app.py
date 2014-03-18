1816d7f1158da94804610ea88459f997f22ba1e4
Jeff Walker <walker.jeff.d@gmail.com>
2014-03-18 15:00:39 -0400

    added watershed model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os.path
 import sys
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import admin
 from flask.ext.admin.contrib import sqla
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 # configure admin
-admin = admin.Admin(app, 'Example')
+admin = admin.Admin(app, 'WIWM')
 
 # Models
 class User(db.Model):
@@ -23,36 +23,67 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
 
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
     def __unicode__(self):
         return self.email
 
+class Watershed(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    usgs_station = db.Column(db.String(120), nullable=False)
+    ghcnd_station = db.Column(db.String(120), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+# routes
 @app.route('/')
 def index():
-  return '<a href="/admin/">Click me to get to Admin!</a>'
+    return render_template('index.html')
+
+@app.route('/watersheds')
+def watershed_list():
+    watersheds = Watershed.query.all()
+    return render_template('watershed_list.html', watersheds=watersheds)
+
+@app.route('/watersheds/<int:id>')
+def watershed_detail(id):
+    watershed = Watershed.query.get(id)
+    return render_template('watershed_detail.html', watershed=watershed)
 
 # admin views
 admin.add_view(sqla.ModelView(User, db.session))
+admin.add_view(sqla.ModelView(Watershed, db.session))
 
 # initialize
-def build_sample_db():
-      print app.config['SQLALCHEMY_DATABASE_URI']
-      db.drop_all()
-      db.create_all()
-      user = User('walker.jeff.d@gmail.com', 'password')
-      db.session.add(user)
-      db.session.commit()
-      return
+users = [('walker.jeff.d@gmail.com', 'password')]
+watersheds = [('Westfield River', '01181000', 'USC00199972')]
 
+def build_sample_db():
+    print app.config['SQLALCHEMY_DATABASE_URI']
+    db.drop_all()
+    db.create_all()
+    for email, password in users:
+        user = User()
+        user.email = email
+        user.password = password
+        db.session.add(user)
+
+    for name, usgs_station, ghcnd_station in watersheds:
+        watershed = Watershed()
+        watershed.name = name
+        watershed.usgs_station = usgs_station
+        watershed.ghcnd_station = ghcnd_station
+        db.session.add(watershed)
+
+    db.session.commit()
+    return
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'clean':
         build_sample_db()
 
     if not os.path.exists(app.config['DATABASE_PATH']):
+        print 'Building database'
         build_sample_db()
 
     app.run(debug=True)
