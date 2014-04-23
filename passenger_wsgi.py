2dfcb89e67f55df947e5a3d6f0941d16ca4f3f7b
Jeff Walker <walker.jeff.d@gmail.com>
2014-04-23 13:45:47 -0400

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'living.walkerjeff.com', 'env', 'bin', 'python')
-
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
- 
-from living.app import app as application
